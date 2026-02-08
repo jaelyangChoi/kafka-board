3f82f16d8480ebeea6e63e71a32bd1cb91cd7150
Jealyang <jaelyangchoi@gmail.com>
2026-02-08 14:57:35 +0900

    feat: 좋아요 구현

---
+++ service/like/src/main/java/kafka/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,13 @@
+package kafka.board.like.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kafka.board.like.entity.ArticleLike;
+
+@Repository
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+	Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
